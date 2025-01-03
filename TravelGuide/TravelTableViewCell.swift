474d0166e80a6efe88a1956cc659e306990fcd74
young <zerobugpark@gmail.com>
2025-01-03 20:31:58 +0900

    Initial Commit

---
+++ TravelGuide/TravelTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  TravelTableViewCell.swift
+//  TravelGuide
+//
+//  Created by youngkyun park on 1/3/25.
+//
+
+import UIKit
+
+class TravelTableViewCell: UITableViewCell {
+
+
+}
---
+++ TravelGuide/TravelTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  TravelTableViewController.swift
+//  TravelGuide
+//
+//  Created by youngkyun park on 1/3/25.
+//
+
+import UIKit
+
+class TravelTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+
+
+
+}
