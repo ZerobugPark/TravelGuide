ae09f648fa278299c0df6af61286dd829f0c791a
young <zerobugpark@gmail.com>
2025-01-07 01:37:07 +0900

    코드최적화

--- TravelGuide/TravelTableViewCell.swift
+++ TravelGuide/TravelTableViewCell.swift
@@ -10,13 +10,69 @@ import Kingfisher
 
 
 class TravelTableViewCell: UITableViewCell {
-
-
+    
+    
+    
     @IBOutlet var travelImageView: UIImageView!
     @IBOutlet var titleLabel: UILabel!
     @IBOutlet var subtitleLabel: UILabel!
     @IBOutlet var dateLabel: UILabel!
     
+    static let identifier = "TravelTableViewCell"
     
+    private let formatter = DateFormatter()
     
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setup()
+    }
+    
+    private func setup() {
+        travelImageView?.contentMode = .scaleAspectFill
+        travelImageView.layer.cornerRadius = 15
+        travelImageView.clipsToBounds = true
+        
+        
+        titleLabel.numberOfLines = 0
+        titleLabel.font = .boldSystemFont(ofSize: 20)
+        
+        subtitleLabel.font = .systemFont(ofSize: 14)
+        subtitleLabel.textColor = .gray
+        subtitleLabel.numberOfLines = 0
+        
+        dateLabel.font = .systemFont(ofSize: 12)
+        dateLabel.textColor = .gray
+    }
+    
+    func configureData(row: Magazine) {
+        
+        let image = row.photo_image
+        if let image {
+            let url = URL(string: image)
+            travelImageView?.kf.setImage(with: url)
+        } else {
+            travelImageView?.image = UIImage(systemName: "star")
+        }
+        
+        titleLabel.text = row.title
+        subtitleLabel.text = row.subtitle
+        let inputFormat = "yyMMdd"
+        let outputFormat = "yy년 MM월 dd일"
+        let formattedDate  = formatDate(from: row.date, inputFormat: inputFormat, outputFormat: outputFormat)
+        
+        dateLabel.text = formattedDate
+    }
+    
+    private func formatDate(from input: String, inputFormat: String, outputFormat: String) -> String {
+        
+        formatter.dateFormat = inputFormat
+        if let date = formatter.date(from: input) {
+            formatter.dateFormat = outputFormat
+            return formatter.string(from: date)
+        } else {
+            formatter.dateFormat = outputFormat
+            return formatter.string(from: Date())
+        }
+        
+    }
 }
--- TravelGuide/TravelTableViewController.swift
+++ TravelGuide/TravelTableViewController.swift
@@ -12,18 +12,19 @@ class TravelTableViewController: UITableViewController {
 
     
     let travelInfo = MagazineInfo().magazine
-    let formatter = DateFormatter()
+    
 
     @IBOutlet var headerLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+    
         tableView.rowHeight = 500
         tableView.separatorStyle = .none //테이블뷰 라인 없애기
        
+        let headerText = "YOUNG TRAVEL"
         headerLabel.font = .boldSystemFont(ofSize: 18)
-        headerLabel.text = "YOUNG TRAVEL"
+        headerLabel.text = headerText
         headerLabel.textAlignment = .center
     }
     
@@ -33,67 +34,17 @@ class TravelTableViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        let cell = tableView.dequeueReusableCell(withIdentifier: "TravelTableViewController", for: indexPath) as! TravelTableViewCell
+        let cell = tableView.dequeueReusableCell(withIdentifier: TravelTableViewCell.identifier, for: indexPath) as! TravelTableViewCell
         
         let row = travelInfo[indexPath.row]
         
-        setImage(magazine: row, cell: cell)
-        setTitle(magazine: row, cell: cell)
-        setDate(magazine: row, cell: cell)
+        cell.configureData(row: row)
 
         
         return cell
     }
     
-    private func setImage(magazine row: Magazine, cell:TravelTableViewCell) {
-        
-        let image = row.photo_image
-        
-        if let image {
-            let url = URL(string: image)
-            cell.travelImageView?.kf.setImage(with: url)
-        } else {
-            cell.travelImageView?.image = UIImage(systemName: "star")
-        }
-        cell.travelImageView?.contentMode = .scaleAspectFill
-        cell.travelImageView.layer.cornerRadius = 15
-        cell.travelImageView.clipsToBounds = true
-    }
-    
-    private func setTitle(magazine row: Magazine, cell:TravelTableViewCell) {
-       
-        cell.titleLabel.text = row.title
-        cell.titleLabel.numberOfLines = 0
-        cell.titleLabel.font = .boldSystemFont(ofSize: 20)
-        
-        cell.subtitleLabel.text = row.subtitle
-        cell.subtitleLabel.font = .systemFont(ofSize: 14)
-        cell.subtitleLabel.textColor = .gray
-        cell.subtitleLabel.numberOfLines = 0
-    }
 
-    private func setDate(magazine row: Magazine, cell:TravelTableViewCell) {
-      
-        let formattedDate  = formatDate(from: row.date, inputFormat: "yyMMdd", outputFormat: "yy년 MM월 dd일")
-        
-        cell.dateLabel.text = formattedDate
-        cell.dateLabel.font = .systemFont(ofSize: 12)
-        cell.dateLabel.textColor = .gray
-        
-    }
-
-    private func formatDate(from input: String, inputFormat: String, outputFormat: String) -> String {
-        
-        formatter.dateFormat = inputFormat
-        if let date = formatter.date(from: input) {
-            formatter.dateFormat = outputFormat
-            return formatter.string(from: date)
-        } else {
-            formatter.dateFormat = outputFormat
-            return formatter.string(from: Date())
-        }
-        
-    }
 
 
 }
